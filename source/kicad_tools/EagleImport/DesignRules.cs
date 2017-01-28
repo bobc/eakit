9ce82ca8db9a22aedadbb987befa797fe9f2e151
Bob Cousins <bobcousins42@googlemail.com>
2017-01-28 18:04:42 +0000

    Add EagleFile, DesignRules classes

---
+++ source/kicad_tools/EagleImport/DesignRules.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EagleImport
+{
+    public class DesignRules
+    {
+        // *** NB : all sizes in mm ***
+
+        int psElongationLong;   ///< percent over 100%.  0-> not elongated, 100->twice as wide as is tall
+                                ///< Goes into making a scaling factor for "long" pads.
+
+        int psElongationOffset; ///< the offset of the hole within the "long" pad.
+
+        float rvPadTop;           ///< top pad size as percent of drill size
+        // double   rvPadBottom;        ///< bottom pad size as percent of drill size
+
+        float rlMinPadTop;        ///< minimum copper annulus on through hole pads
+        float rlMaxPadTop;        ///< maximum copper annulus on through hole pads
+
+        float rvViaOuter;         ///< copper annulus is this percent of via hole
+        float rlMinViaOuter;      ///< minimum copper annulus on via
+        float rlMaxViaOuter;      ///< maximum copper annulus on via
+        float mdWireWire;         ///< wire to wire spacing I presume.
+
+
+        public DesignRules()
+        {
+            psElongationLong = 100;
+            psElongationOffset = 0;
+            rvPadTop = 0.25f;
+            // rvPadBottom = 0.25f;
+            rlMinPadTop = 0.254f;  // = 10mil
+            rlMaxPadTop = 0.508f;  // = 20mil
+            rvViaOuter = 0.25f;
+            rlMinViaOuter = 0.254f;  // = 10mil
+            rlMaxViaOuter = 0.508f;  // = 20mil
+            mdWireWire = 0;
+        }
+
+        public float CalcPadSize (float drill)
+        {
+            return 2 * (drill * rvPadTop) + drill;
+        }
+
+    }
+}
---
+++ source/kicad_tools/EagleImport/EagleFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Xml.Serialization;
+using System.IO;
+
+namespace EagleImport
+{
+    [XmlRoot(ElementName = "eagle")]
+    public class EagleFile
+    {
+        // TODO: maybe not quite right
+        [XmlElement(ElementName = "compatibility")]
+        public List<Compatibility> Compatibility { get; set; }
+
+        [XmlElement(ElementName = "drawing")]
+        public Drawing Drawing { get; set; }
+
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+
+        public static EagleFile LoadFromXmlFile(string FileName)
+        {
+            EagleFile result = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(EagleFile));
+            if (!File.Exists(FileName))
+                return result;
+            FileStream fs = new FileStream(FileName, FileMode.Open);
+            try
+            {
+                result = (EagleFile)serializer.Deserialize(fs);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+            return result;
+        }
+
+    }
+}
