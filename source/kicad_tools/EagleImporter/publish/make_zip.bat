2f5b7635f81fefad88bbdebfa2750ee5c0f44b79
Bob Cousins <bobcousins42@googlemail.com>
2017-02-01 14:57:49 +0000

    Create 0_1_0_4 release

--- source/kicad_tools/EagleImporter/publish/make_zip.bat
+++ source/kicad_tools/EagleImporter/publish/make_zip.bat
@@ -4,9 +4,12 @@ rem
 rem make package zip
 rem
 
+set version=%1
+
+set release_dir=..\..\..\..\release
+
 set zip=pkzip.exe
-set zzzname=eakit
-set version=0_1_0_1
+set zzzname=eakit_%version%
 rem set flags=-silent
 set flags=
 
@@ -15,7 +18,10 @@ del %zzzname%.zip
 %zip% -add %flags% %zzzname%.zip EagleImporter.application setup.exe "Application Files\EagleImporter_%version%\*.*"  -Path -recurse
 
 echo Calculating sum..
-CertUtil -hashfile %zzzname%.zip SHA256 >hash.txt
+CertUtil -hashfile %zzzname%.zip SHA256 >hash_%version%.txt
+
+copy %zzzname%.zip  %release_dir%
+copy hash_%version%.txt %release_dir%
 
 set zip=
 set zzzname=
